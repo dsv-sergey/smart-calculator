3a409b7b9685128097c0064aa845bdccb47b9288
Derevyannik Sergey <33173033+dsv-sergey@users.noreply.github.com>
2018-10-04 21:25:52 +0300

    smart-calculator ok

--- src/index.js
+++ src/index.js
@@ -1,26 +1,35 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.result = eval('' + initialValue);
   }
 
   add(number) {
-    // your implementation
+    this.result += '+' + number;
+    return this;
   }
   
   subtract(number) {
-    // your implementation
+    this.result += '-' + number;
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.result += '*' + number;
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.result += '/' + number;
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.result += '**' + number;
+    return this;
+  }
+  
+  valueOf() {
+    return eval(this.result);
   }
 }
 
